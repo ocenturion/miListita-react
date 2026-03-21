8d4e79977e6903d1760f7417fc1c9a7958e1386b
Oscar Centurion <centurion.oscar90@gmail.com>
2026-03-21 01:47:08 -0300

    mejoras en los estilos

--- miListita-react/src/App.tsx
+++ miListita-react/src/App.tsx
@@ -1,35 +1,88 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+interface Articulo {
+  detalle: string;
+  cantidad: number;
+  precio: number;
 }
 
-export default App
+function App() {
+  const [articulos, setArticulos] = useState<Articulo[]>([]);
+  const [nuevoArticulo, setNuevoArticulo] = useState("");
+  const [precio, setPrecio] = useState(0);
+  const [cantidad, setCantidad] = useState(1);
+
+  const AgregarArticulo = () => {
+    if (nuevoArticulo.trim() === "") return;
+
+    const nuevo: Articulo = {
+      detalle: nuevoArticulo,
+      cantidad,
+      precio,
+    };
+    setArticulos([...articulos, nuevo]);
+    setNuevoArticulo("");
+  };
+
+  const total = articulos.reduce((acumulador, articulo) => {
+    return acumulador + articulo.cantidad * articulo.precio;
+  }, 0);
+
+  useEffect(() => {
+    // console.log(`Articulos actualizados: ${articulos}`);
+  }, [articulos]);
+
+  return (
+    <div className="container">
+      <label htmlFor="">Articulo:</label>
+      <input
+        type="text"
+        value={nuevoArticulo}
+        placeholder="Articulo"
+        onChange={(e) => {
+          setNuevoArticulo(e.target.value);
+        }}
+      />
+      <label htmlFor="">Precio:</label>
+      <input
+        type="number"
+        value={precio}
+        placeholder="Precio"
+        onChange={(e) => setPrecio(Number(e.target.value))}
+      />
+      <label htmlFor="">Cantidad:</label>
+      <input
+        type="number"
+        value={cantidad}
+        placeholder="Cantidad"
+        onChange={(e) => setCantidad(Number(e.target.value))}
+      />
+      <button onClick={AgregarArticulo}>Agregar</button>
+
+      <table>
+        <thead>
+          <tr>
+            <th>Descripcion</th>
+            <th>Precio</th>
+          </tr>
+        </thead>
+        <tbody>
+          {articulos.map((articulo, index) => (
+            <tr key={index}>
+              <td>
+                <p>{articulo.detalle}</p>
+                <p>{articulo.cantidad} x {articulo.precio}</p>
+              </td>
+              <td>${articulo.cantidad * articulo.precio}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+
+      <h3>Total: ${total}</h3>
+    </div>
+  );
+}
+
+export default App;
